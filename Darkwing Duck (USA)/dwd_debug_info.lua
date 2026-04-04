b9b496a7a4abbd2a8e4ff57369b527aac599bb71
Omkommersind <chauzov@list.ru>
2026-04-04 14:45:45 +0300

    dwd collision box

--- Darkwing Duck (USA)/dwd_debug_info.lua
+++ Darkwing Duck (USA)/dwd_debug_info.lua
@@ -3,6 +3,8 @@ local xLow <const> = 0x04F0
 local xHigh <const> = 0x0500
 local yLow <const> = 0x0510
 local yHigh <const> = 0x0520
+local xScreen <const> = 0x0450
+local yScreen <const> = 0x0440
 
 local fg <const> = 0xFFFFFF
 local bg <const> = 0x0000AA
@@ -34,6 +36,24 @@ local function drawLookDirection()
     drawString(8, 212, string.format("Looking: %s", dir), fg, bg)
 end
 
+local function drawCollisionBox()
+	x = read(xScreen, memNES, false)
+	y = read(yScreen, memNES, false)
+
+    emu.drawRectangle(x - 12, y + 10, 24, 2, 0x00AA00)
+end
+
+local function drawHurtBox()
+    -- TODO
+	x = read(xScreen, memNES, false)
+	y = read(yScreen, memNES, false)
+    
+    emu.drawRectangle(x - 12, y - 16, 24, 32, 0xCC0000)
+end
+    
+
 emu.addEventCallback(drawPlayerCoords, emu.eventType.endFrame)
+emu.addEventCallback(drawCollisionBox, emu.eventType.endFrame)
+--emu.addEventCallback(drawHurtBox, emu.eventType.endFrame)
 -- emu.addEventCallback(drawLookDirection, emu.eventType.endFrame)
 emu.displayMessage("Script", "Darkwing Duck debug viewer loaded.")
